1c2e7b059b7b673a5f67dbae319a2a6eb92d9a80
shivam0626 <shivammaurya7310@gmail.com>
2022-11-15 18:09:17 +0530

    navbar updated

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,14 +6,16 @@ const Navbar = () =>{
     return(
         <Box columns={[1,null,2]} className={styles.main_navbar} id="top">
             <Box >
-                <Link  to="#"><Text className={styles.logo}>ShivamM</Text></Link>
+                <Link  to="#"><Text className={styles.logo}>S H I V A M</Text></Link>
             </Box>
             <Box className={styles.nav_box2}>
-                <Link to="#" smooth><Text size={"sm"}>Home</Text></Link>
-                <Link to="#about" smooth><Text size={"sm"}>About me</Text></Link>
+                <Box>
+                    <Link to="#" smooth><Text size={"sm"}>Home</Text></Link>
+                </Box>
+                <Link to="#about" smooth><Text size={"sm"}>About</Text></Link>
                 <Link to="#skills" smooth><Text size={"sm"}>Skills</Text></Link>
                 <Link to="#projects" smooth><Text size={"sm"}>Projects</Text></Link>
-                <Link to="#contact" smooth><Text size={"sm"}>Contact me</Text></Link>
+                <Link to="#contact" smooth><Text size={"sm"}>Contact</Text></Link>
                 <a  href={"https://drive.google.com/file/d/1N8u-FjHCRGhXKcjtRTBCPSbmEzpBHqxl/view?usp=sharing"} target ="blank" ><button className={styles.resumeBtn}>Resume</button></a>
             </Box>
         </Box>
