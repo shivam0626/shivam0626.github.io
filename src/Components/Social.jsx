c74d948ac7bfc5e04c5d1e559ebc376a085b1f1a
shivam0626 <shivammaurya7310@gmail.com>
2023-03-21 12:50:50 +0530

    resume link updated

--- src/Components/Social.jsx
+++ src/Components/Social.jsx
@@ -11,7 +11,7 @@ export const Social = () => {
         <a href="https://github.com/shivam0626" target="blank"><FaGithub /></a>
         <a href="https://www.linkedin.com/in/shivam-maurya-5131331b6/" target="blank"><FaLinkedin /></a>
         <a href="https://twitter.com/ShivamM96073793" target="blank"><FaTwitter /></a>
-        <a  href={"https://drive.google.com/file/d/1N8u-FjHCRGhXKcjtRTBCPSbmEzpBHqxl/view?usp=sharing"} target="blank" ><button className={styles.resumeBtn}><ImProfile /></button></a>
+        <a  href={"https://drive.google.com/file/d/1xqGpp6pdVQCrkOKQI14I0549FJzz_4Hi/view?usp=sharing"} target="blank" ><button className={styles.resumeBtn}><ImProfile /></button></a>
     </Box>
     <Box>
       <Text fontSize={"1.5rem"}>Phone: +91-7310941087</Text>
